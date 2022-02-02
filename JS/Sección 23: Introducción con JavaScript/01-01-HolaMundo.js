336f243fc874f9dc8fdfda8faf8485583e15ca79
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-02 07:51:41 -0500

    Sección 23 y 24 del curso

---
+++ JS/Sección 23: Introducción con JavaScript/01-01-HolaMundo.js
@@ -0,0 +1,3 @@
+var nombre = "Hola Mundo";
+
+console.log(nombre);
---
+++ JS/Sección 24: Tipos de Datos en JavaScript/02-01-TiposDatosJavaScript.js
@@ -0,0 +1,23 @@
+/*
+Ejemplos de tipos de datos en Javascript
+*/ 
+
+//Tipo de datos string
+var nombre = "Jarlin";
+console.log(nombre);
+
+//Tipo de dato numerico
+var numero = 1000;
+console.log(numero);
+
+//Tipo de dato object
+var objeto ={
+    nombre : "Juan",
+    apellido: "Peres",
+    telefono: "3124544564"
+}
+
+console.log(objeto);
+
+var num = 10;
+console.log(typeof num);
