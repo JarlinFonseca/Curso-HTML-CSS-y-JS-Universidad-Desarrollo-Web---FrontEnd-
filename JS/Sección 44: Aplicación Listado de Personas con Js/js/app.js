99524c35a649f78fdff9288e63b1b432f53d1afc
Jarlin Fonseca <jarlinandres5000@gmail.com>
2022-02-08 10:47:49 -0500

    Sección 44 del curso

---
+++ JS/Sección 44: Aplicación Listado de Personas con Js/js/app.js
@@ -0,0 +1,30 @@
+const personas =[
+    new Persona('Juan', 'Perez'),
+    new Persona('Karla', 'Lara')
+];
+
+function mostrarPersonas(){
+    console.log('Mostrar personas...');
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersonas(){
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if(nombre.value != '' && apellido.value != ''){
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona);
+        mostrarPersonas();
+    }else{
+        alert('No hay información que agregar')
+        console.log('No hay información que agregar');
+    }
+    
+}
